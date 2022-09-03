ca132a2b7e248ec0f73935a00254d4f833f632b2
musawakiliML <musawakiliml@hotmail.com>
2022-09-03 20:04:48 +0100

    Update

--- python_repos.py
+++ python_repos.py
@@ -17,18 +17,18 @@ repo_dicts = response_dict['items']
 print(f"Repositories Returned: {len(repo_dicts)}")
 
 # Examine the first repo
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
+#repo_dict = repo_dicts[0]
+#print(f"\nKeys: {len(repo_dict)}")
 
 # for key in sorted(repo_dict.keys()):
 #   print(key)
 
-
-print(f"\n Selected Information About First Repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for repo_dict in repo_dicts:
+    print(f"\n Selected Information About First Repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
